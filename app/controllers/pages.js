60f4feda3b7c90b1126ce07df37e0c0cbf246a13
Eric Martindale <eric@ericmartindale.com>
2015-07-03 14:31:21 -0700

    delete app folder. :)

--- app/controllers/pages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  examples: function(req, res, next) {
-    require('fs').readFile('data/examples.json', function(err, data) {
-      res.provide( 'examples', {
-        examples: JSON.parse(data)
-      });
-    });
-  }
-}
