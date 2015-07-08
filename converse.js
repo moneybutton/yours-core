7e6cdd1ab095a9f1cff5cbe3de74eab38bae7b9c
Eric Martindale <eric@ericmartindale.com>
2015-07-08 16:41:49 -0700

    Begin to migrate to a loose format.

--- converse.js
+++ converse.js
@@ -20,7 +20,7 @@ converse.define('Person', {
   icon: 'user'
 });
 
-converse.define('Forum', {
+converse.define('Board', {
   attributes: {
     name: { type: String, required: true, max: 200 , slug: true },
     description: { type: String, max: 1024 },
@@ -29,24 +29,26 @@ converse.define('Forum', {
     _owner: { type: ObjectId, required: true, ref: 'Person' },
     _moderators: [ { type: ObjectId, ref: 'Person' } ]
   },
-  icon: 'list'
-});
-
-converse.define('Topic', {
-  attributes: {
-    name: { type: String, required: true, max: 200 },
-    description: { type: String, required: true },
-    _author: { type: ObjectId, required: true, ref: 'Person' },
-    _forum:  { type: ObjectId, /* required: true, */ ref: 'Forum' },
-    created: { type: Date, required: true, default: Date.now }
-  },
-  icon: 'pin'
+  icon: 'slack'
 });
 
 converse.define('Post', {
   attributes: {
+    name: { type: String, required: true, max: 200 },
+    description: { type: String, required: true },
     _author: { type: ObjectId, required: true, ref: 'Person' },
-    _topic: { type: ObjectId, required: true },
+    _board:  { type: ObjectId, /* required: true, */ ref: 'Forum' },
+    created: { type: Date, required: true, default: Date.now },
+    link:    { type: String }
+  },
+  icon: 'pin'
+});
+
+converse.define('Comment', {
+  attributes: {
+    _author: { type: ObjectId, required: true, ref: 'Person' },
+    _post: { type: ObjectId, required: true , ref: 'Post' },
+    _parent: { type: ObjectId, required: true , ref: 'Comment' },
     created: { type: Date, required: true, default: Date.now },
     updated: { type: Date },
     content: { type: String, min: 1 }
