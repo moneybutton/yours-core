b0b90c1ee204452d66ec0f438460bc54aef38e3b
Eric Martindale <eric@ericmartindale.com>
2015-08-06 00:21:41 -0700

    Migrate over to DATT branding.

--- db.js
+++
@@ -1,8 +0,0 @@
-var config = require('./config')
-var mongoose = require('mongoose');
-
-/* connect mongoose to the database */
-mongoose.connect( config.database.uri , config.database.name );
-
-/* export a copy of mongoose */
-exports.mongoose = mongoose;
