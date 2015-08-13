ac9fc2705e92487c3a1eeb9f7bb0942fc234b49d
halseth <johanth@gmail.com>
2015-08-12 23:24:28 -0700

    Added some basic functionality to show how local storing and announcement of content could be implemented (using localStorag for the browser for now).

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var bitcore = require('bitcore')
 var Peer = require('./lib/peer')
 
 var Content = require('./lib/content')
+var ContentStore = require('./lib/contentStore')
 var User = require('./lib/user')
 var Message = require('./lib/message')
 var Datt = require('./lib/datt')
 
 module.exports = {
   'Content': Content,
+  'ContentStore': ContentStore,
   'User': User,
   'Message': Message,
   'Datt': Datt,
