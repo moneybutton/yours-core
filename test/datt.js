9a30e222825f6958ca90db01f262012328b1e16e
Ryan X. Charles <ryanxcharles@gmail.com>
2015-08-14 21:42:51 -0700

    promisify part of lib/datt.js iface & add tests

--- test/datt.js
+++ test/datt.js
@@ -17,4 +17,38 @@ describe('Datt', function () {
 
   })
 
+  // TODO: Enable the rest of these tests in node by creating code with the
+  // same interface that works over TCP instead of Web RTC. Or, optionally, the
+  // server-side code could also use the node-webrtc library.
+  if (!process.browser) {
+    return
+  }
+
+  describe('#begin', function () {
+
+    it('should initialize our global datt', function () {
+      return datt.begin()
+    })
+
+  })
+
+  describe('#signIn', function () {
+
+    it('should sign in a user', function () {
+      return datt.signIn('user', 'password').then(function (user) {
+        user.username.should.equal('user')
+        user.password.should.equal('password')
+      })
+    })
+
+  })
+
+  describe('#broadcastMessage', function () {
+
+    it('should return a promise', function () {
+      return datt.broadcastMessage('my message')
+    })
+
+  })
+
 })
