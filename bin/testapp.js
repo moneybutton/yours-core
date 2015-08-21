11e7757c3dd4da4855e4153d4960c9c83b409060
Ryan X. Charles <ryanxcharles@gmail.com>
2015-08-20 17:58:23 -0700

    make workers work in browser

---
+++ bin/testapp.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/**
+ * Basic web app to facilitate running the mocha browser tests without karma.
+ */
+var express = require('express')
+var path = require('path')
+var app = express()
+var ExpressPeerServer = require('peer').ExpressPeerServer
+
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../node_modules')))
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Run the mocha tests at http://%s:%s/tests.html', host, port)
+})
+
+app.use('/', ExpressPeerServer(server, {debug: true}))
+
+module.exports.app = app
+module.exports.server = server
