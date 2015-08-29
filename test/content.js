2166707bf292bd3a66cd221d329756d4d78de213
Willy Bruns <willy@entropi.info>
2015-08-28 20:15:32 -0700

    Fix Content tests which were not really tests ;-)

--- test/content.js
+++ test/content.js
@@ -432,9 +432,9 @@ describe('Content', function () {
       var data = 'test data ftw'
 
       Content.fromDataAndUser(data, newUser).then(function (newContent) {
-        newContent.getOwnerUsername() === newUser.getUsername()
-        newContent.getOwnerAddress() === newUser.getAddress()
-        newContent.getOwnerPubKey() === newUser.getPubKey()
+        newContent.getOwnerUsername().should.eql(newUser.getUsername())
+        newContent.getOwnerAddress().should.eql(newUser.getAddress())
+        newContent.getOwnerPubKey().should.eql(newUser.getPubKey())
         done()
       })
 
@@ -445,7 +445,7 @@ describe('Content', function () {
 
       var data = 'test data ftw'
       Content.fromDataAndUser(data, newUser).then(function (newContent) {
-        newContent.getData() === data
+        newContent.getData().should.eql(data)
         done()
       })
     })
@@ -470,7 +470,7 @@ describe('Content', function () {
       var data = 'test data ftw'
       var pt = new Date()
       Content.fromDataAndUser(data, newUser, pt).then(function (nc2) {
-        nc2.getPostTime() === pt.toString()
+        nc2.getPostTime().should.eql(pt.toString())
         done()
       })
     })
@@ -482,7 +482,7 @@ describe('Content', function () {
       var pt = new Date()
       var ph = 305000
       Content.fromDataAndUser(data, newUser, pt, ph).then(function (nc3) {
-        nc3.getPostHeight() === ph
+        nc3.getPostHeight().should.eql(ph)
         done()
       })
     })
