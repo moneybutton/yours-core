d11941b707deeba004a63e6eaf88e12f672cfc6c
Willy Bruns <willy@entropi.info>
2015-08-29 20:50:11 -0700

    ContentStore enhancements and test updates
    - ContentStore#getContentByUsername: finish impl and add tests
    - Test for ContentStore#getContentHashes that claims to check that array of hashes is returned actually checks that all elements are strings

--- test/contentStore.js
+++ test/contentStore.js
@@ -6,7 +6,9 @@ var Content = require('../lib/content')
 var util = require('../lib/util')
 var os = require('os')
 var tmpdir = os.tmpdir()
+
 var q = require('q')
+var u = require('underscore')
 
 describe('ContentStore', function () {
   this.timeout(15000) // karma seems to need longer timeout for IndexedDB tests
@@ -59,6 +61,51 @@ describe('ContentStore', function () {
 
   })
 
+  describe('#getContentByUsername', function () {
+    it('should return an array', function () {
+      return contentStore.getContentByUsername('username').then(function (result) {
+        should.exist(result)
+        should(result instanceof Array).be.eql(true)
+      })
+    })
+
+    it('should return an empty array when a username without associated content is provided', function () {
+      return contentStore.getContentByUsername('a_random_username_with_no_content')
+	    .catch(function (err) {
+		should.fail("Should not throw error: " + err + " \n\n " + err.stack)
+	    })
+	    .then(function (result) {
+        should.exist(result)
+        should(result instanceof Array).be.eql(true)
+        should(result.length).be.eql(0)
+      })
+    })
+
+    it('should return content with the provided username', function () {
+      return contentStore.getContentByUsername(content.getOwnerUsername()).then(function (result) {
+        should.exist(result)
+        should(result.length).be.eql(1)
+        var retrievedContent = result[0]
+        should(retrievedContent.getHashHex()).be.eql(content.getHashHex())
+	should(retrievedContent.getOwnerUsername()).be.eql(content.getOwnerUsername())
+      })
+    })
+
+    it('should reject the promise if username is undefined, null, or not a string', function () {
+	return q.allSettled([
+	    contentStore.getContentByUsername(), 
+	    contentStore.getContentByUsername(null), 
+	    contentStore.getContentByUsername({'type': "wrong"})
+	]).spread(function (undefinedUsername, nullUsername, wrongTypeUsername) {
+	   undefinedUsername.state.should.eql('rejected') 
+	   nullUsername.state.should.eql('rejected')
+	   wrongTypeUsername.state.should.eql('rejected')
+	})
+    })
+  })
+
+  describe('#getContentByUserAddress', function () {})
+
   describe('#getContentHashes', function () {
     it('should return an array', function () {
       return contentStore.getContentHashes().then(function (hashes) {
@@ -67,23 +114,29 @@ describe('ContentStore', function () {
       })
     })
 
-    it('should return an array of strings, each one a Content hash', function () {
-	contentStore.putContent(content).then(function () {
-	    return contentStore.getContentHashes()
-	}).catch(function (err) {
-	    should.fail("Should not throw error: " + err + " \n\n " + (err || {}).stack)
-	}).then(function (hashes) {
-	    should(hashes.indexOf(hashHexString)).not.eql(-1)
-	})
+    it('should return an array of strings, each one a Content hash, which includes the Content we inserted earlier', function () {
+      contentStore.putContent(content).then(function () {
+        return contentStore.getContentHashes()
+      }).catch(function (err) {
+        should.fail('Should not throw error: ' + err + ' \n\n ' + (err || {}).stack)
+      }).then(function (hashes) {
+	var stringElements = u.filter(
+	    u.map(hashes, function(el) { return typeof(el) }), 
+	    function(typeofel) { return typeofel === 'string' }
+	)
+	stringElements.length.should.eql(hashes.length)
+
+        should(hashes.indexOf(hashHexString)).not.eql(-1)
+      })
     })
 
     it('should return an array with a count equal to the number of docs in the PouchDB minus the number of indices (2)', function () {
-	q.all([contentStore.db.info(), contentStore.getContentHashes()])
-	    .spread(function (info, hashes) {
-		should.exist(hashes)
-		should.exist(info)
-		should(hashes.length).be.eql(info.doc_count - 2)
-	    })
+      q.all([contentStore.db.info(), contentStore.getContentHashes()])
+        .spread(function (info, hashes) {
+          should.exist(hashes)
+          should.exist(info)
+          should(hashes.length).be.eql(info.doc_count - 2)
+        })
     })
 
     it('should return an empty array without error if the content store is empty', function () {
@@ -93,7 +146,6 @@ describe('ContentStore', function () {
       }).catch(function (err) {
         should.fail('Should not throw an error: ' + err + ' \n\n ' + (err || {}).stack)
       }).then(function (hashes) {
-        console.log(arguments)
         should.exist(hashes)
         should(hashes instanceof Array).be.eql(true)
         should(hashes.length).be.eql(0)
