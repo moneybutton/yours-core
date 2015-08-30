d03f29af41b3f9b0b2991cff17876a41c648759d
Ryan X. Charles <ryanxcharles@gmail.com>
2015-08-29 21:35:35 -0700

    add test for fromSeed

--- test/walletfile.js
+++ test/walletfile.js
@@ -13,13 +13,26 @@ describe('WalletFile', function () {
 
   })
 
-  describe('fromRandom', function () {
+  describe('@fromRandom', function () {
     it('should make a new walletfile', function () {
       return WalletFile.fromRandom().then(function (walletfile) {
         should.exist(walletfile)
         should.exist(walletfile.mnemonic)
         should.exist(walletfile.masterxprv)
         should.exist(walletfile.masterxpub)
+      })
+    })
+
+  })
+
+  describe('@fromSeed', function () {
+    it('should derive new mnemonic, xprv, xpub', function () {
+      var seedbuf = new Buffer(128 / 8)
+      seedbuf.fill(0)
+      return WalletFile.fromSeed(seedbuf).then(function (walletfile) {
+        walletfile.mnemonic.should.equal('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about')
+        walletfile.masterxprv.toString().should.equal('xprv9s21ZrQH143K3GJpoapnV8SFfukcVBSfeCficPSGfubmSFDxo1kuHnLisriDvSnRRuL2Qrg5ggqHKNVpxR86QEC8w35uxmGoggxtQTPvfUu')
+        walletfile.masterxpub.toString().should.equal('xpub661MyMwAqRbcFkPHucMnrGNzDwb6teAX1RbKQmqtEF8kK3Z7LZ59qafCjB9eCRLiTVG3uxBxgKvRgbubRhqSKXnGGb1aoaqLrpMBDrVxga8')
       })
     })
 
