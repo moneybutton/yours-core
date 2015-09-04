6e111453e16650d2d085fa6bf76391025af016c8
Ryan X. Charles <ryanxcharles@gmail.com>
2015-09-03 20:01:31 -0700

    refactor content discovery tests like normal
    Tests should be divided per-function. I've formatted the content discovery
    tests to be for the handleContentDiscoveryRequest function.

--- test/main.js
+++ test/main.js
@@ -18,8 +18,7 @@ describe('Main', function () {
 
   })
 
-  describe('ContentDiscovery', function () {
-    // Create a temporary to be used during tests
+  describe('#handleContentDiscoveryRequest', function () {
     function getMainObject () {
       var main = new Main()
       main.getContent = function (hash) {
@@ -43,65 +42,60 @@ describe('Main', function () {
       return main
     }
 
-    describe('ContentDiscovery', function () {
-      var main = getMainObject()
+    var mainMock = getMainObject()
 
-      describe('#handleContentDiscoveryRequest', function () {
-        it('should not handle message coming from itself', function () {
-          var message = Message.requestPeersForHash('testhash', main.peer.id)
-          return main.handleContentDiscoveryRequest(message).then(function (result) {
-            result.should.equal('Own message')
-          })
-        })
-
-        it('should handle message with another id and send an announcement because it has this content', function () {
-          main.peer.id = '1'
-          var message = Message.requestPeersForHash('testhash', main.peer.id)
-          main.peer.id = '0'
-          return main.handleContentDiscoveryRequest(message).then(function (result) {
-            result.should.eql(['broadcasted', 'sentMessage'])
-          })
-        })
-
-        it('should not propagate the same message twice', function () {
-          main.peer.id = '1'
-          var message = Message.requestPeersForHash('testhash', main.peer.id)
-          main.peer.id = '0'
-          return main.handleContentDiscoveryRequest(message).then(function (result) {
-            result.should.equal('Already seen message. Stop propagating')
-          })
-        })
-
-        it('should propagate message and not send announcement', function () {
-          main.peer.id = '1'
-          var message = Message.requestPeersForHash('testhash2', main.peer.id)
-          main.peer.id = '0'
-          return main.handleContentDiscoveryRequest(message).then(function (result) {
-            result.should.eql(['broadcasted', 'Did not have content'])
-          })
-        })
-      })
-
-      it('should throw an error if it cannot send announcement', function () {
-        // Create main object with failing send
-        var main2 = getMainObject()
-        main2.sendMessage = function (msg, sender) {
-          var deferred = q.defer()
-          deferred.reject('send error')
-          return deferred.promise
-        }
-        main2.peer.id = '2'
-        var message = Message.requestPeersForHash('testhash', main2.peer.id)
-        main2.peer.id = '0'
-
-        return main2.handleContentDiscoveryRequest(message).then(function (result) {
-          should.fail('ContentDiscover#handleContentDiscoveryRequest should fail when unable to send message')
-        }).fail(function (error) {
-          error.should.equal('send error')
-        })
+    it('should not handle message coming from itself', function () {
+      var message = Message.requestPeersForHash('testhash', mainMock.peer.id)
+      return mainMock.handleContentDiscoveryRequest(message).then(function (result) {
+        result.should.equal('Own message')
       })
     })
 
+    it('should handle message with another id and send an announcement because it has this content', function () {
+      mainMock.peer.id = '1'
+      var message = Message.requestPeersForHash('testhash', mainMock.peer.id)
+      mainMock.peer.id = '0'
+      return mainMock.handleContentDiscoveryRequest(message).then(function (result) {
+        result.should.eql(['broadcasted', 'sentMessage'])
+      })
+    })
+
+    it('should not propagate the same message twice', function () {
+      mainMock.peer.id = '1'
+      var message = Message.requestPeersForHash('testhash', mainMock.peer.id)
+      mainMock.peer.id = '0'
+      return mainMock.handleContentDiscoveryRequest(message).then(function (result) {
+        result.should.equal('Already seen message. Stop propagating')
+      })
+    })
+
+    it('should propagate message and not send announcement', function () {
+      mainMock.peer.id = '1'
+      var message = Message.requestPeersForHash('testhash2', mainMock.peer.id)
+      mainMock.peer.id = '0'
+      return mainMock.handleContentDiscoveryRequest(message).then(function (result) {
+        result.should.eql(['broadcasted', 'Did not have content'])
+      })
+    })
+
+    it('should throw an error if it cannot send announcement', function () {
+      // Create mainMock object with failing send
+      var mainMock2 = getMainObject()
+      mainMock2.sendMessage = function (msg, sender) {
+        var deferred = q.defer()
+        deferred.reject('send error')
+        return deferred.promise
+      }
+      mainMock2.peer.id = '2'
+      var message = Message.requestPeersForHash('testhash', mainMock2.peer.id)
+      mainMock2.peer.id = '0'
+
+      return mainMock2.handleContentDiscoveryRequest(message).then(function (result) {
+        should.fail('ContentDiscover#handleContentDiscoveryRequest should fail when unable to send message')
+      }).fail(function (error) {
+        error.should.equal('send error')
+      })
+    })
   })
 
   // TODO: Enable the rest of these tests in node by creating code with the
