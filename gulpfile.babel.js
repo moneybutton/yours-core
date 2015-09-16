9361837170501cd9d9b1f39bdd8fdb43b2b1a689
Ryan X. Charles <ryanxcharles@gmail.com>
2015-09-15 17:19:00 -0700

    re-enable app; works in node 4.0
    I've managed to make the app work on my machine by fixing some firewall issue
    that was breaking bower. I've also made the app work in node 4.0 by fixing some
    minor issues, as well as removing the browser-sync dependency which seemed to
    be broken in node 4.0, and is also not necessary.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,10 @@
 // generated on 2015-07-09 using generator-gulp-webapp 1.0.2
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
-import browserSync from 'browser-sync';
 import del from 'del';
 import {stream as wiredep} from 'wiredep';
 
 const $ = gulpLoadPlugins();
-const reload = browserSync.reload;
 
 var stylus = require('gulp-stylus');
 
@@ -16,7 +14,6 @@ gulp.task('styles', () => {
     .pipe($.autoprefixer({browsers: ['last 1 version']}))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
 });
 
 gulp.task('scripts', function () {
@@ -29,16 +26,14 @@ gulp.task('views', function () {
   return gulp.src('app/*.jade')
     .pipe($.jade({pretty: true}))
     .pipe(gulp.dest('.tmp'))
-    .pipe(reload({stream: true}));
 });
 
 function lint(files, options) {
   return () => {
     return gulp.src(files)
-      .pipe(reload({stream: true, once: true}))
       .pipe($.eslint(options))
       .pipe($.eslint.format())
-      .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+      .pipe($.eslint.failAfterError());
   };
 }
 const testLintOptions = {
@@ -70,17 +65,6 @@ gulp.task('html', ['views', 'styles', 'scripts'], () => {
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
-    .pipe($.if($.if.isFile, $.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    }))
-    .on('error', function (err) {
-      console.log(err);
-      this.end();
-    })))
     .pipe(gulp.dest('images'));
 });
 
@@ -103,61 +87,6 @@ gulp.task('extras', () => {
 });
 
 gulp.task('clean', del.bind(null, ['.tmp']));
-
-gulp.task('serve', ['views', 'styles', 'fonts', 'scripts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch([
-    'app/*.html',
-    '.tmp/*.html',
-    'app/scripts/**/*.js',
-    'app/images/**/*',
-    '.tmp/fonts/**/*',
-    '.tmp/scripts/**/*.js',
-  ]).on('change', reload);
-
-  gulp.watch('app/**/*.jade', ['views']);
-  gulp.watch('app/styles/**/*.styl', ['styles']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('app/scripts/**/*.coffee', ['scripts', reload]);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
-});
-
-gulp.task('serve:dist', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist']
-    }
-  });
-});
-
-gulp.task('serve:test', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
-});
 
 // inject bower components
 gulp.task('wiredep', () => {
