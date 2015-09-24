a828f3023a0253199647b71324c890e76c84a702
Ryan X. Charles <ryanxcharles@gmail.com>
2015-09-24 15:23:58 -0700

    move front-end entry point to views/
    Code layout:
    - All server-side executables are in bin/*
    - Front-end code is in views/*
    - Isomorphic p2p code (works both in node and browser) is in lib/*
    - Tests are in test/* , but currently only test the lib/* code

--- bin/app.js
+++
@@ -1,5 +0,0 @@
-var React = require('react')
-var StatusBox = require('../views/index.jsx')
-
-var statusbox = <StatusBox propname='Main StatusBox'/>
-React.render(statusbox, document.getElementById('container'))
---
+++ views/app.js
@@ -0,0 +1,6 @@
+'use strict'
+let React = require('react')
+let StatusBox = require('./index.jsx')
+
+let statusbox = <StatusBox propname='Main StatusBox'/>
+React.render(statusbox, document.getElementById('container'))
