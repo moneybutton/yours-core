55857ebf4ca0c3866b0ceb0700c99c3576e95e08
Ryan X. Charles <ryanxcharles@gmail.com>
2015-09-25 15:14:54 -0700

    full test coverage for DB

--- test/db.js
+++ test/db.js
@@ -1,20 +1,39 @@
-/* global describe,it,after */
+/* global describe,it,before,after */
 'use strict'
 let DB = require('../lib/db')
 let should = require('should')
 
 describe('DB', function () {
+  let name = 'testdatabase'
   let db
+
+  before(function () {
+    db = DB(name)
+    return db.init()
+  })
 
   after(function () {
     return db.close()
   })
 
-  it('should open a database', function () {
-    let name = 'testdatabase'
-    db = DB(name)
-    return db.init().then(function (info) {
-      should.exist(info)
+  describe('#init', function () {
+    it('should exist', function () {
+      should.exist(db.init)
+    })
+  })
+
+  describe('#close', function () {
+    it('should exist', function () {
+      should.exist(db.close)
+    })
+  })
+
+  describe('#info', function () {
+    it('should give some info', function () {
+      return db.info().then(function (info) {
+        should.exist(info)
+        should.exist(info.db_name)
+      })
     })
   })
 })
