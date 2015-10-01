8587ed5c9912a2a9efcbc56492836002e443b835
Ryan X. Charles <ryanxcharles@gmail.com>
2015-09-30 20:12:11 -0700

    slightly improved error handling in gulpfile
    There was a problem that when you produce a syntax error in a .jsx file, the
    watchify build in "gulp serve" breaks and you can't keep refreshing and must
    fix the error, and then restart "gulp serve" to be able to see the changes.
    I've fixed this by erasing and re-creating the bundle upon an error. This seems
    to fix things ... sometimes. I can produce a syntax error, fix it, and then
    things refresh and work now.
    
    However, this approach is not really desirable because I have to recreate the
    watchify object. This works but I believe it is slow. Also, it still seems to
    work inconsistently. I will have to continue investigating.

--- gulpfile.js
+++ gulpfile.js
@@ -75,14 +75,20 @@ gulp.task('build-workerpool', function () {
 
 browserifyOpts = {debug: false}
 browserifyOpts = Object.assign({}, watchify.args, browserifyOpts)
-let build_worker_browserify = watchify(browserify(browserifyOpts))
-build_worker_browserify
-  // The polyfill needs to be included exactly once per page. We put it in
-  // the worker and in the bundle.
-  .add(require.resolve('babelify/polyfill'))
-  .transform(envify)
-  .transform(babelify)
-  .add(require.resolve('./lib/worker.js'), {entry: true})
+let build_worker_browserify
+
+function set_build_worker_browserify () {
+  build_worker_browserify = watchify(browserify(browserifyOpts))
+  build_worker_browserify
+    // The polyfill needs to be included exactly once per page. We put it in
+    // the worker and in the bundle.
+    .add(require.resolve('babelify/polyfill'))
+    .transform(envify)
+    .transform(babelify)
+    .add(require.resolve('./lib/worker.js'), {entry: true})
+}
+
+set_build_worker_browserify()
 
 function build_worker () {
   return new Promise(function (resolve, reject) {
@@ -91,6 +97,8 @@ function build_worker () {
         .bundle()
         .on('error', function (err) {
           gutil.log.bind(gutil, 'Browserify Error')
+          build_worker_browserify.close()
+          set_build_worker_browserify()
           reject(err)
         })
         .pipe(fs.createWriteStream(path.join(__dirname, 'build', process.env.DATT_CORE_JS_WORKER_FILE)))
@@ -106,15 +114,21 @@ gulp.task('build-worker', ['build-workerpool'], function () {
 
 browserifyOpts = {debug: false}
 browserifyOpts = Object.assign({}, watchify.args, browserifyOpts)
-let build_core_browserify = watchify(browserify(browserifyOpts))
-build_core_browserify
-  .on('error', gutil.log.bind(gutil, 'Unknown Error'))
-  // The polyfill needs to be included exactly once per page. We put it in
-  // the worker and in the bundle.
-  .add(require.resolve('babelify/polyfill'))
-  .transform(envify)
-  .transform(babelify)
-  .require(require.resolve('./lib/index.js'), {entry: true})
+let build_core_browserify
+
+function set_build_core_browserify () {
+  build_core_browserify = watchify(browserify(browserifyOpts))
+  build_core_browserify
+    .on('error', gutil.log.bind(gutil, 'Unknown Error'))
+    // The polyfill needs to be included exactly once per page. We put it in
+    // the worker and in the bundle.
+    .add(require.resolve('babelify/polyfill'))
+    .transform(envify)
+    .transform(babelify)
+    .require(require.resolve('./lib/index.js'), {entry: true})
+}
+
+set_build_core_browserify()
 
 function build_core () {
   return new Promise(function (resolve, reject) {
@@ -123,6 +137,8 @@ function build_core () {
         .bundle()
         .on('error', function (err) {
           gutil.log.bind(gutil, 'Browserify Error')
+          build_core_browserify.close()
+          set_build_core_browserify()
           reject(err)
         })
         .pipe(fs.createWriteStream(path.join(__dirname, 'build', process.env.DATT_CORE_JS_BUNDLE_FILE)))
@@ -138,12 +154,18 @@ gulp.task('build-core', ['build-worker'], function () {
 
 browserifyOpts = {debug: false}
 browserifyOpts = Object.assign({}, watchify.args, browserifyOpts)
-let build_react_browserify = watchify(browserify(browserifyOpts))
-build_react_browserify
-  // Do not include the polyfill - it is already included by datt-core.js
-  .transform('reactify')
-  .transform(babelify)
-  .add(require.resolve('./views/index.js'), {entry: true})
+let build_react_browserify
+
+function set_build_react_browserify () {
+  build_react_browserify = watchify(browserify(browserifyOpts))
+  build_react_browserify
+    // Do not include the polyfill - it is already included by datt-core.js
+    .transform('reactify')
+    .transform(babelify)
+    .add(require.resolve('./views/index.js'), {entry: true})
+}
+
+set_build_react_browserify()
 
 function build_react () {
   let p1 = new Promise(function (resolve, reject) {
@@ -158,6 +180,8 @@ function build_react () {
         .bundle()
         .on('error', function (err) {
           gutil.log.bind(gutil, 'Browserify Error')
+          build_react_browserify.close()
+          set_build_react_browserify()
           reject(err)
         })
         .pipe(fs.createWriteStream(path.join(__dirname, 'build', process.env.DATT_REACT_JS_FILE)))
@@ -176,22 +200,29 @@ gulp.task('build-react', function () {
 
 browserifyOpts = {debug: false}
 browserifyOpts = Object.assign({}, watchify.args, browserifyOpts)
-let build_tests_browserify = watchify(browserify(browserifyOpts))
+let build_tests_browserify
 
-gulp.task('build-tests-prebundle', function (callback) {
-  glob('./test/**/*+(.js|.jsx)', {}, function (err, files) {
-    if (err) {
-      callback(err)
-      return
-    }
-    build_tests_browserify
-      .transform(envify)
-      .transform(babelify)
-    for (let file of files) {
-      build_tests_browserify.add(file)
-    }
-    callback()
+function set_build_tests_browserify () {
+  return new Promise(function (resolve, reject) {
+    glob('./test/**/*+(.js|.jsx)', {}, function (err, files) {
+      build_tests_browserify = watchify(browserify(browserifyOpts))
+      if (err) {
+        reject(err)
+        return
+      }
+      build_tests_browserify
+        .transform(envify)
+        .transform(babelify)
+      for (let file of files) {
+        build_tests_browserify.add(file)
+      }
+      resolve()
+    })
   })
+}
+
+gulp.task('build-tests-prebundle', function () {
+  return set_build_tests_browserify()
 })
 
 function build_tests () {
@@ -201,7 +232,12 @@ function build_tests () {
         .bundle()
         .on('error', function (err) {
           gutil.log.bind(gutil, 'Browserify Error')
-          reject(err)
+          build_tests_browserify.close()
+          set_build_tests_browserify().then(function () {
+            reject(err)
+          }).catch(function (error) {
+            reject(err + ', ' + error)
+          })
         })
         .pipe(fs.createWriteStream(path.join(__dirname, 'build', process.env.DATT_JS_TESTS_FILE)))
         .on('finish', resolve)
