7a81cf1fb55b2b0c07ad5f38d9dc6ba8a9e19825
Ryan X. Charles <ryanxcharles@gmail.com>
2015-10-04 13:49:59 -0700

    DBUser
    So that you can save the in-memory user to the database and get it later, so
    that when you refresh the browser you don't start over with a new identity.

---
+++ test/lib/db-user.js
@@ -0,0 +1,51 @@
+/* global describe,it,before,after */
+'use strict'
+let DB = require('../../lib/db')
+let DBUser = require('../../lib/db-user')
+let User = require('../../lib/user')
+let should = require('should')
+
+describe('DBUser', function () {
+  let db = DB('datt-testdatabase')
+  let user = User().fromRandom()
+
+  before(function () {
+    return db.init().then(() => {
+      return User().fromRandom()
+    }).then(_user => {
+      user = _user
+    })
+  })
+
+  after(function () {
+    return db.destroy()
+  })
+
+  it('should exist', function () {
+    should.exist(DBUser)
+    should.exist(DBUser())
+    should.exist(DBUser(db, user))
+  })
+
+  describe('#save', function () {
+    it('should save a dbuser', function () {
+      let dbuser = DBUser(db, user)
+      return dbuser.save().then(() => {
+        dbuser.user.mnemonic.should.equal(user.mnemonic)
+      })
+    })
+  })
+
+  describe('#get', function () {
+    it('should get a dbuser', function () {
+      let dbuser = DBUser(db, user)
+      return dbuser.save().then(() => {
+        dbuser.user.mnemonic.should.equal(user.mnemonic)
+        return DBUser(db).get()
+      }).then(user => {
+        should.exist(user)
+        user.mnemonic.should.equal(dbuser.user.mnemonic)
+      })
+    })
+  })
+})
