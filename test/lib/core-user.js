adc8a8f3785374da1f379db616b24aa320de0542
Ryan X. Charles <ryanxcharles@gmail.com>
2015-10-04 15:14:20 -0700

    CoreUser
    I'm iterating towards breaking up the functionality of dattcore into "core"
    units, one of which is CoreUser. dattcore will have a coreuser object that
    manages the in-memory user and database user.

---
+++ test/lib/core-user.js
@@ -0,0 +1,61 @@
+/* global describe,it,before,after */
+'use strict'
+let DB = require('../../lib/db')
+let CoreUser = require('../../lib/core-user')
+let should = require('should')
+let MsgAuth = require('../../lib/msg-auth')
+let BR = require('fullnode/lib/br')
+
+describe('CoreUser', function () {
+  let db = DB('datt-testdatabase')
+  let coreuser = CoreUser(db)
+  let blockidhex = '00000000000000000e6188a4cc93e3d3244b20bfdef1e9bd9db932e30f3aa2f1'
+  let blockhashbuf = BR(new Buffer(blockidhex, 'hex')).readReverse()
+  let blockheightnum = 376949
+
+  before(function () {
+    return db.init()
+  })
+
+  after(function () {
+    return db.destroy()
+  })
+
+  it('should exist', function () {
+    should.exist(CoreUser)
+    should.exist(CoreUser())
+  })
+
+  describe('#init', function () {
+    it('should initialize a new user', function () {
+      return coreuser.init().then(coreuser => {
+        coreuser.user.name.should.equal('satoshi')
+        coreuser.dbuser.user.name.should.equal('satoshi')
+        should.exist(coreuser.user.mnemonic)
+        should.exist(coreuser.user.masterxprv)
+        should.exist(coreuser.user.masterxpub)
+      })
+    })
+  })
+
+  describe('#asyncSetName', function () {
+    it('should set the name to a new name', function () {
+      let name = 'newname'
+      return coreuser.asyncSetName(name).then(coreuser => {
+        coreuser.user.name.should.equal(name)
+        coreuser.dbuser.user.name.should.equal(name)
+      })
+    })
+  })
+
+  describe('#asyncGetMsgAuth', function () {
+    it('should return a valid msgauth', function () {
+      return coreuser.asyncGetMsgAuth(blockhashbuf, blockheightnum).then(msgauth => {
+        ;(msgauth instanceof MsgAuth).should.equal(true)
+        return msgauth.asyncVerify()
+      }).then(verified => {
+        verified.should.equal(true)
+      })
+    })
+  })
+})
