d13b8fc7628d6fb866b3c0159499dd68747f46e9
Ryan X. Charles <ryanxcharles@gmail.com>
2015-10-10 13:33:37 -0700

    peers.broadcastMsg; test ping/pong works

--- test/lib/peers.js
+++ test/lib/peers.js
@@ -1,6 +1,7 @@
 /* global describe,it,before,after */
 'use strict'
 let Peers = require('../../lib/peers')
+let MsgPing = require('../../lib/msg-ping')
 let spawn = require('../../lib/spawn')
 let should = require('should')
 
@@ -11,7 +12,7 @@ describe('Peers', function () {
   }
 
   let peers
-  let Network
+  let Network, network2
   if (process.browser) {
     Network = require('../../lib/network-browser-webrtc')
   } else {
@@ -29,17 +30,36 @@ describe('Peers', function () {
   })
 
   after(function () {
+    network2.close()
     peers.close()
   })
 
   describe('#asyncConnect', function () {
     it('should be able to connect to a peer in the same process', function () {
       return spawn(function *() {
-        let network2 = Network()
+        network2 = Network()
         yield network2.asyncInitialize()
-        let obj = yield peers.asyncConnect(network2.getConnectionInfo())
-        should.exist(obj.connection)
-        should.exist(obj.network)
+        let pair = yield peers.asyncConnect(network2.getConnectionInfo())
+        should.exist(pair.connection)
+        should.exist(pair.network)
+      })
+    })
+  })
+
+  describe('#broadcastMsg', function () {
+    it('should send ping and get pong', function () {
+      return spawn(function *() {
+        let msgPing = MsgPing().fromRandom()
+        let msg = msgPing.toMsg()
+        let network1 = peers.networks.webrtc // TODO: Test should work in node also
+        let connection = network1.connections[0]
+        yield new Promise((resolve, reject) => {
+          connection.once('msg', msg => {
+            msg.getCmd().should.equal('pong')
+            resolve()
+          })
+          peers.broadcastMsg(msg)
+        })
       })
     })
   })
