d1d60340c3d6dd44f1dffa8170da2f98f2ac97eb
Ryan X. Charles <ryanxcharles@gmail.com>
2015-10-19 16:30:00 -0700

    save emails to file

--- bin/landing-page.js
+++ bin/landing-page.js
@@ -3,12 +3,20 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 let path = require('path')
+let fs = require('fs')
 let app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../static')))
 app.post('/', (req, res) => {
-  console.log(req.body)
+  let str = JSON.stringify(req.body)
+  fs.appendFile('./landing-page-form-submit.dat', str, (err) => {
+    console.log(str)
+    if (err) {
+      console.log('error on form submit: ' + err)
+    }
+  })
+  res.sendFile('index.html', {root: path.join(__dirname, '../static')})
 })
 
 let server = app.listen(3050, function () {
