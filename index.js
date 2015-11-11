34971d2c8b38fe282b9f698f183285c0377c8bb6
Ryan X. Charles <ryanxcharles@gmail.com>
2015-11-11 09:12:05 -0800

    comment about what entry point is

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+/**
+ * The default export is DattCore, the logic of the datt p2p protocol. DattCore
+ * is what you want to use to run a datt node, either in node.js or a web
+ * browser. It does not include the UI.
+ */
 module.exports = require('./core/index')
