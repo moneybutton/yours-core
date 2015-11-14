273749352cdfdeaa2563d7d0a290e2e0f67abc21
Ryan X. Charles <ryanxcharles@gmail.com>
2015-11-14 12:01:15 -0800

    BIP 44 Account
    This is an "account" in the sense of BIP44, which is a particular way of
    deriving key addresses with BIP32. This class will be used by the upcoming
    BIP44Wallet class which will let you manage multiple accounts.
    
    This code probably properly belongs in fullnode and not in Datt, however
    because it uses AsyncCrypto, it will be in Datt for now. fullnode has an
    entirely differnent way of managing workers which I may ultimately change to be
    more like Datt. In any case, for now this code goes here.

---
+++ core/bip44-account.js
@@ -0,0 +1,77 @@
+/**
+ * BIP44 Account
+ * =============
+ *
+ * According to BIP44, an account is a xpub or xprv from which you can derive
+ * the chain index and then address index. If you have the private key, you can
+ * derive the other private keys. If you have the public key, you can only
+ * derive public keys and addresses.
+ */
+'use strict'
+let Struct = require('fullnode/lib/struct')
+let AsyncCrypto = require('./async-crypto')
+let spawn = require('../util/spawn')
+
+function BIP44Account (bip32, addrindex, changeindex, keymap) {
+  if (!(this instanceof BIP44Account)) {
+    return new BIP44Account(bip32, addrindex, changeindex, keymap)
+  }
+  this.initialize()
+  this.fromObject({bip32, addrindex, changeindex, keymap})
+}
+
+BIP44Account.prototype = Object.create(Struct.prototype)
+BIP44Account.prototype.constructor = BIP44Account
+
+BIP44Account.prototype.initialize = function () {
+  this.keymap = new Map()
+  this.addrindex = -1
+  this.changeindex = -1
+  return this
+}
+
+BIP44Account.prototype.isPrivate = function () {
+  return this.bip32.isPrivate()
+}
+
+BIP44Account.prototype.asyncDeriveKeysFromPath = function (path) {
+  return spawn(function *() {
+    if (!path) {
+      throw new Error('must specify path - see bip32 specification for format')
+    }
+    let keys
+    keys = this.keymap.get(path)
+    if (keys) {
+      return keys
+    }
+    if (this.bip32.isPrivate()) {
+      keys = yield AsyncCrypto.deriveXkeysFromXprv(this.bip32, path)
+    } else {
+      keys = yield AsyncCrypto.deriveXkeysFromXpub(this.bip32, path)
+    }
+    this.keymap.set(path, keys)
+    return keys
+  }.bind(this))
+}
+
+BIP44Account.prototype.asyncGetNextAddressKeys = function () {
+  return spawn(function *() {
+    let addrindex = this.addrindex + 1
+    let path = 'm/0/' + addrindex
+    let keys = yield this.asyncDeriveKeysFromPath(path)
+    this.addrindex = addrindex
+    return keys
+  }.bind(this))
+}
+
+BIP44Account.prototype.asyncGetNextChangeKeys = function () {
+  return spawn(function *() {
+    let changeindex = this.changeindex + 1
+    let path = 'm/1/' + changeindex
+    let keys = yield this.asyncDeriveKeysFromPath(path)
+    this.changeindex = changeindex
+    return keys
+  }.bind(this))
+}
+
+module.exports = BIP44Account
---
+++ test/core/bip44-account.js
@@ -0,0 +1,73 @@
+/* global describe,it */
+'use strict'
+let BIP44Account = require('../../core/bip44-account')
+let BIP32 = require('fullnode/lib/bip32')
+let should = require('should')
+let spawn = require('../../util/spawn')
+
+describe('BIP44Account', function () {
+  it('should exist', function () {
+    should.exist(BIP44Account)
+    should.exist(BIP44Account())
+  })
+
+  describe('#isPrivate', function () {
+    it('should know if the bip32 does or does not have the privkey', function () {
+      let bip32 = BIP32().fromRandom()
+      let bip44account = BIP44Account(bip32)
+      bip44account.isPrivate().should.equal(true)
+      bip32 = bip32.toPublic()
+      bip44account = BIP44Account(bip32)
+      bip44account.isPrivate().should.equal(false)
+    })
+  })
+
+  describe('#asyncDeriveKeysFromPath', function () {
+    it('should derive keys', function () {
+      return spawn(function *() {
+        let bip32 = BIP32().fromRandom()
+        let bip44account = BIP44Account(bip32)
+        let keys = yield bip44account.asyncDeriveKeysFromPath('m/0')
+        should.exist(keys.xprv)
+        should.exist(keys.xpub)
+        should.exist(keys.address)
+        bip32 = bip32.toPublic()
+        bip44account = BIP44Account(bip32)
+        keys = yield bip44account.asyncDeriveKeysFromPath('m/0')
+        should.not.exist(keys.xprv)
+        should.exist(keys.xpub)
+        should.exist(keys.address)
+      })
+    })
+  })
+
+  describe('#asyncGetNextAddressKeys', function () {
+    it('should derive next address', function () {
+      return spawn(function *() {
+        let bip32 = BIP32().fromRandom()
+        let bip44account = BIP44Account(bip32)
+        let keys = yield bip44account.asyncGetNextAddressKeys()
+        should.exist(keys.xprv)
+        should.exist(keys.xpub)
+        should.exist(keys.address)
+        let keys2 = yield bip44account.asyncGetNextAddressKeys()
+        keys2.xprv.toString().should.not.equal(keys.xprv.toString())
+      })
+    })
+  })
+
+  describe('#asyncGetNextChangeKeys', function () {
+    it('should derive next address', function () {
+      return spawn(function *() {
+        let bip32 = BIP32().fromRandom()
+        let bip44account = BIP44Account(bip32)
+        let keys = yield bip44account.asyncGetNextChangeKeys()
+        should.exist(keys.xprv)
+        should.exist(keys.xpub)
+        should.exist(keys.address)
+        let keys2 = yield bip44account.asyncGetNextChangeKeys()
+        keys2.xprv.toString().should.not.equal(keys.xprv.toString())
+      })
+    })
+  })
+})
