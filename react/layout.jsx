e7303c19ff66b98989198d8fbce81e06ada97f8e
Willy Bruns <willy@entropi.info>
2015-12-06 19:58:07 -0500

    0th pass - Adding non-functional menu from mockup

--- react/layout.jsx
+++ react/layout.jsx
@@ -71,6 +71,22 @@ let Layout = React.createClass({
             <img src='/logo.svg' alt='' />
             <h1>{this.props.apptitle}</h1>
           </div>
+	  <div className='topMenu col-md-8 col-md-offset-3' id='header'>
+	  <ul className='menu'>
+	  <li className='selected'>VIEW ALL<img src='images/arrow.png'/></li>
+	  <li>MAIL</li>
+	  <li>SETTINGS</li>
+	  <li>BALANCE<span className='balance'>(0.0112)</span></li>
+	  </ul>
+
+
+	  <ul className='icons'>
+	  <li><img src='images/icon_new.png' /></li>
+	  <li><img src='images/icon_search.png' /></li>
+	  </ul>
+	  </div>
+
+	  
         </div>
 
         <div className='row'>
