aea3bfb30df13e3bbfd42ecc522569df9ee65b4f
Ryan X. Charles <ryanxcharles@gmail.com>
2015-12-10 18:05:22 -0800

    fix proxy
    The proxy I was using, express-request-proxy, was a very sophisticated tool
    that seemed to mostly work fine, except that it wasn't relaying POST messages
    correctly. This was causing blockchain API POST queries not to work in the
    browser. I'm not sure what the problem was, but I got things to work using
    http-proxy instead, which is a much simpler tool with fewer features. I also
    had to fix the new "local" scripts I created, since they were setting the wrong
    environment variable.

--- server/app.js
+++ server/app.js
@@ -8,40 +8,38 @@
 'use strict'
 let express = require('express')
 let path = require('path')
-let requestProxy = require('express-request-proxy')
+let httpProxy = require('http-proxy')
 let config = require('../config')
 
 module.exports.createAppServer = function createAppServer (port) {
   let app = express()
 
   // The blockchain API proxy.
-  app.get('/blockchain-api/:val1/:val2/:val3', requestProxy({
-    url: config.DATT_BLOCKCHAIN_API_URI + ':val1/:val2/:val3'
-  }))
-  app.get('/blockchain-api/:val1/:val2', requestProxy({
-    url: config.DATT_BLOCKCHAIN_API_URI + ':val1/:val2'
-  }))
-  app.get('/blockchain-api/:val1', requestProxy({
-    url: config.DATT_BLOCKCHAIN_API_URI + ':val1'
-  }))
-  app.post('/blockchain-api/:val1/:val2/:val3', requestProxy({
-    url: config.DATT_BLOCKCHAIN_API_URI + ':val1/:val2/:val3'
-  }))
-  app.post('/blockchain-api/:val1/:val2', requestProxy({
-    url: config.DATT_BLOCKCHAIN_API_URI + ':val1/:val2'
-  }))
-  app.post('/blockchain-api/:val1', requestProxy({
-    url: config.DATT_BLOCKCHAIN_API_URI + ':val1'
-  }))
+  let apiProxy = httpProxy.createProxyServer()
+  app.all('/blockchain-api/*', (req, res) => {
+    req.url = req.url.replace('/blockchain-api/', '/')
+    apiProxy.web(req, res, {
+      target: config.DATT_BLOCKCHAIN_API_URI,
+      changeOrigin: true
+    })
+  })
 
   // The front-end is just static HTML, CSS and JS files.
   app.use(express.static(path.join(__dirname, '../build')))
 
-  let server = app.listen(port, function () {
+  let server = app.listen(port, () => {
     let host = server.address().address
     let port = server.address().port
     console.log('View the app at http://%s:%s/', host, port)
     console.log('Run the mocha browser tests at http://%s:%s/tests.html', host, port)
   })
+
+  // Be sure also to close the proxy server when "closing"
+  server.oldClose = server.close
+  server.close = () => {
+    server.oldClose()
+    apiProxy.close()
+  }
+
   return server
 }
